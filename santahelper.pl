00121cd0cbd56a3e4278135e927c998cb52575b2
Joseph <56702275+joeguy57@users.noreply.github.com>
2020-12-05 00:33:49 -0700

    Update santahelper.pl

--- santahelper.pl
+++ santahelper.pl
@@ -7,22 +7,22 @@
 %
 % The following facts are known about the reindeer and the elves who
 % rode them that day:
-% 1. Rudolf hangs out two or more areas to the eastof Wunorse Openslae�s reindeer.
+% 1. Rudolf hangs out two or more areas to the eastof Wunorse Openslae's reindeer.
 % 2. The reindeer of the elf that made nine toys lives somewhere to the
-% west of Alabaster Snowball�s reindeer.
+% west of Alabaster Snowball's reindeer.
 % 3. Three reindeer in consecutive areas, from west to east, are Dasher,
-% Alabaster Snowball�s reindeer, and the reindeer of the elf that made
+% Alabaster Snowball's reindeer, and the reindeer of the elf that made
 % 12 toys.
 % 4. The elf who rode Rudolf has made three toys more than the
 % one who rode the reindeer in area 4, while Pepper Minstix made three
 % toys more than Sugarplum Mary. All four elves mentioned here are
 % unique.
-% 5. Comet�s rider made three toys more than Shinny Upatree, who
+% 5. Comet's rider made three toys more than Shinny Upatree, who
 % in turn made two toys more than the elf who rode Vixen. The elf who
 % rode Vixen is Shinny Upatree, Sugarplum Mary or Wunorse Onenslae.
-% 6. Blitzen lives somewhere to the west of Bushy Evergreen�s reindeer.
+% 6. Blitzen lives somewhere to the west of Bushy Evergreen's reindeer.
 % 7. Alabaster Snowball made exactly one toy more than Wunorse Openslae.
-% 8. Pepper Minstix didn�t ride the reindeer from area 6.
+% 8. Pepper Minstix didn't ride the reindeer from area 6.
 %
 %Created By: Arnold Gihzo and Joseph Menezes
 %Created on 2nd December 2020
@@ -64,7 +64,7 @@ rule5a([_,_,["Vixen", "Wunrose Openslae",_,_],_,_,_]).
 % This rule is broken down into two section as the data provided is
 % unique.
 %
-% Comet�s rider made three toys more than Shinny Upatree, who in
+% Comet's rider made three toys more than Shinny Upatree, who in
 % turn made two toys more than the elf who rode Vixen. The elf who rode
 % Vixen is Shinny Upatree, Sugarplum Mary or Wunorse Onenslae.
 %
@@ -76,7 +76,7 @@ rule5(Solution) :-
     member([_,"Shinny Upatree", ShinnyToys,_], Solution),
     member(["Vixen",_,VixenToys,_], Solution),
     CometToys is ShinnyToys + 3,
-    CometToys is VixenToys + 5.
+    ShinnyToys is VixenToys + 3.
 
 %===========================================================
 % distributionAreasAndToys - it will put areas and toys into the Solutions
@@ -185,7 +185,8 @@ uniqueElf([[_,A1,_,_],[_,A2,_,_],[_,A3,_,_],[_,A4,_,_],[_,A5,_,_],[_,A6,_,_]]):-
 %then runs list through the rules.
 % hey Arnold look at the rules below unique toys they need to be
 % optimized, Their not ordered in a way that would give rule 5
-% the best possibility of truth
+% the best possibility of truth, maybe above toys also needs to be ordered 
+% in a way
 %
 solve(Solution):-
   solutions(Solution),
